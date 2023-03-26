96465c9e858b60c362b021cad72c27595154ec2e
Tobias <113835194+optobimus@users.noreply.github.com>
2023-03-26 18:36:51 +0200

    Header styling + favicon and tab name

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+import logo from "../logo.svg";
+
 import "../styles/header.css"
 
 function Header() {
   return (
     <div className="header">
+      <img src={logo} alt="Logo"/>
       <h1>Memory Card</h1>
     </div>  
   );
